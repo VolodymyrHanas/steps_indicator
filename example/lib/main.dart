8d080d130734f0ceebc14e176343874db214efd0
VolodymyrHanas <49440930+VolodymyrHanas@users.noreply.github.com>
2020-03-24 22:03:20 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -36,49 +36,123 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            StepsIndicator(
-              selectedStep: selectedStep,
-              nbSteps: nbSteps,
-              doneLineColor: Colors.green,
-              doneStepColor: Colors.green,
-              undoneLineColor: Colors.red,
-              unselectedStepColor: Colors.red,
-            ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                MaterialButton(
-                  color: Colors.red,
-                  onPressed: () {
-                    if(selectedStep > 0) {
-                      setState(() {
-                        selectedStep--;
-                      });
-                    }
-                  },
-                  child: Text('Prev'),
-                ),
-                MaterialButton(
-                  color: Colors.green,
-                  onPressed: () {
-                    if(selectedStep < nbSteps) {
-                      setState(() {
-                        selectedStep++;
-                      });
-                    }
-                  },
-                  child: Text('Next'),
-                )
-              ],
-            )
-          ],
-        )
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          StepsIndicator(
+            selectedStep: selectedStep,
+            lineThickness: 2.0,
+            nbSteps: nbSteps,
+            doneLineColor: Colors.red,
+            undoneLineColor: Colors.grey,
+            unselectedStepWidget: (index) => UnselectedStep(index),
+            selectedStepWidget: (index) => SelectedStep(index),
+            doneStepWidget: DoneStep(),
+          ),
+          Container(height: 50),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              MaterialButton(
+                color: Colors.red,
+                onPressed: () {
+                  if (selectedStep > 0) {
+                    setState(() {
+                      selectedStep--;
+                    });
+                  }
+                },
+                child: Text('Prev'),
+              ),
+              Container(width: 50),
+              MaterialButton(
+                color: Colors.green,
+                onPressed: () {
+                  if (selectedStep < nbSteps) {
+                    setState(() {
+                      selectedStep++;
+                    });
+                  }
+                },
+                child: Text('Next'),
+              )
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
+
+class UnselectedStep extends StatelessWidget {
+  final int index;
+
+  const UnselectedStep(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: Colors.grey,
+          width: 1.0,
+        ),
+      ),
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(color: Colors.grey, fontSize: 10, fontWeight: FontWeight.w500),
+        ),
       ),
     );
   }
 }
+
+class SelectedStep extends StatelessWidget {
+  final int index;
+
+  const SelectedStep(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(color: Colors.white, fontSize: 10, fontWeight: FontWeight.w500),
+        ),
+      ),
+    );
+  }
+}
+
+class DoneStep extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 16,
+      height: 16,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
+      ),
+      child: Center(
+        child: Image.asset("lib/done.png", width: 10, height: 10,)
+      ),
+    );
+  }
+}
+
+
+
